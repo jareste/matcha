d1233d2efcb8b14e6e863fb9c09319d12cf32cf8
jareste- <jareste2000@gmail.com>
2024-03-18 21:43:14 +0100

    modified init

--- back/srcs/__init__.py
+++ back/srcs/__init__.py
@@ -9,14 +9,12 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    # Set the database URI
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 
-    # Initialize the database with the app
     db.init_app(app)
 
     with app.app_context():
-        from . import routes, test, models  # move the import here
+        from . import routes, test, models
         db.create_all()
 
     app.register_blueprint(routes.bp)
