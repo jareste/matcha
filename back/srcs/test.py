5653bf12e355ed2d4d93d6520ab2213d2eb7704c
Joan Areste Gonzalez <jareste2000@gmail.com>
2024-03-30 18:54:09 +0100

    modified photo hash as it was not working as intended

--- back/srcs/test.py
+++ back/srcs/test.py
@@ -22,25 +22,32 @@ def upload_photo():
     if file.filename == '':
         return 'No selected file'
     if file:
+        print(file.filename)
         try:
-            Image.open(file)
+            with Image.open(file.stream) as img:
+                pass  # Just opening to validate
         except Exception as e:
             abort(400, description="The file is not an image")
+        file.stream.seek(0)  # Reset file pointer to the beginning
         filename = secure_filename(file.filename)
-        hashed_filename = hash_to_db(filename)
+        username = 'jareste'
+        unique_filename = f"{username}_{filename}"
+        hashed_filename = hash_to_db(unique_filename) + ".png"
         upload_path = os.path.join(app.config['UPLOAD_FOLDER'], hashed_filename)
         os.makedirs(os.path.dirname(upload_path), exist_ok=True)
         file.save(upload_path)
         photo = Photo()
         photo.insert(user_id='jareste', url=upload_path)
-        print("photo saved")
+        print("photo saved at: ", upload_path)
         return 'File uploaded successfully'
 
 @bp.route('/delete_photo/<photo_url>', methods=['DELETE'])
 def delete_photo(photo_url):
     # Query the database for the photo
     photo = Photo()
-    hashed_filename = hash_to_db(photo_url)
+    username = 'jareste'
+    unique_filename = f"{username}_{photo_url}"
+    hashed_filename = hash_to_db(unique_filename) + ".png"
     upload_path = os.path.join(app.config['UPLOAD_FOLDER'], hashed_filename)
     photo_record = photo.select(url=upload_path)
     if not photo_record:
@@ -64,6 +71,8 @@ from flask import send_from_directory
 
 @bp.route('/uploads/<filename>')
 def uploaded_file(filename):
-    hashed_filename = hash_to_db(filename)
+    username = 'jareste'
+    unique_filename = f"{username}_{filename}"
+    hashed_filename = hash_to_db(unique_filename) + ".png"
     print("showing photo: ", hashed_filename)
     return send_from_directory(app.config['UPLOAD_FOLDER'], hashed_filename)
